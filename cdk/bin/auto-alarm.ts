fb98c0b3641bcbd377cb255fbf2ffe90e1f3bea1
Erik Jensen <365211+erikrj@users.noreply.github.com>
2024-08-04 06:19:19 -0600

    Setting version to come from package.json and refactoring props

--- cdk/bin/auto-alarm.ts
+++ cdk/bin/auto-alarm.ts
@@ -2,7 +2,7 @@
 import 'source-map-support/register';
 import {AutoAlarmStack} from '../lib/auto-alarm-stack';
 import {ExtendedApp} from 'truemark-cdk-lib/aws-cdk';
-import {ExtendedAutoAlarmProps} from '../lib/auto-alarm-stack-props';
+import {version} from '../../package.json';
 
 const app = new ExtendedApp({
   standardTags: {
@@ -20,8 +20,7 @@ const app = new ExtendedApp({
 // The prometheusWorkspaceId const is configured to take in an environment variable for the Prometheus Workspace ID which
 // is then passed to our lambda to use dynamically across all environments.
 const prometheusWorkspaceId = app.node.tryGetContext('prometheusWorkspaceId');
-const stackProps: ExtendedAutoAlarmProps = {
+new AutoAlarmStack(app, 'AutoAlarm', {
+  version,
   prometheusWorkspaceId: prometheusWorkspaceId,
-};
-
-new AutoAlarmStack(app, 'AutoAlarm', stackProps);
+});
